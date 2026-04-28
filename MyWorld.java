47304b5980f60fa32eb03a843e09eb0f8df24205
edelinliu <440009774@gapps.yrdsb.ca>
2026-04-28 11:05:36 -0400

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,22 @@
 import greenfoot.*;
 
-public class MyWorld extends World {
+/**
+ * The World our hero lives in.
+ * 
+ * @author Edelin
+ * @version April 2026
+ */
+
+public class MyWorld extends World 
+{
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
     public MyWorld() {
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
     }
 }
